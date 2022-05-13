366877c35917ffab44a2bf320ade60783fa79e8f
emaf <emanuele.falli@mail.polimi.it>
2022-05-13 19:49:33 +0200

    spo2 calculation is faulty, needs to be adjusted always outputs -999

--- Project_LTEBS/Saturimetro.cydsn/main.c
+++ Project_LTEBS/Saturimetro.cydsn/main.c
@@ -1,11 +1,15 @@
 /**
 *   Main file for testing MAX30101 Library.
+    Last update: il codice printa 25 samples però il calcolo della spo2 e hr non funzionano, sul monitor viene restituito -999 quindi c'è un errore nel calcolo.
+    ho anche notato che se si cambia la risoluzione dai 15 attuali ai 18 bit il codice vede 32 samples come fullFifo e non 25 come gli ho impostato. molto strano non capisco perchè succeda
+    anche se nel codice è impostato il FIFOAFull a 25 
 */
 
 #include "project.h"
 #include "MAX30101.h"
 #include "stdio.h"
 #include "I2C_Interface.h"
+#include "SpO2.h"
 
 #define UART_DEBUG
 
@@ -24,6 +28,7 @@
 CY_ISR_PROTO(MAX30101_ISR);
 
 uint8_t flag_temp = 0;
+uint8_t flag_1s = 0;
 
 int main(void)
 {
@@ -35,6 +40,15 @@ int main(void)
     void (*print_ptr)(const char*) = &(UART_Debug_PutString);
     uint8_t active_leds = 2;
     uint8_t rp, wp, flag = 0;
+    int32_t spo2; //SPO2 value
+    int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+    int32_t heartRate; //heart rate value
+    int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+    uint32_t irBuffer[200]; //infrared LED sensor data
+    uint32_t redBuffer[200];  //red LED sensor data
+    int32_t bufferLength = 200;
+    int num_samples;
+    int k=0;
     
     // Initialization
     MAX30101_Start();
@@ -77,7 +91,7 @@ int main(void)
         MAX30101_EnableFIFOAFullInt();
      
         // set 28 samples to trigger interrupt
-        MAX30101_SetFIFOAlmostFull(28);
+        MAX30101_SetFIFOAlmostFull(25);
 
         // enable fifo rollover
         MAX30101_EnableFIFORollover();
@@ -131,7 +145,7 @@ int main(void)
                 MAX30101_ReadReadPointer(&rp);
                 MAX30101_ReadWritePointer(&wp);
                 //Calculate the number of readings we need to get from sensor
-                int num_samples = wp - rp;
+                num_samples = wp - rp;
                 if (num_samples <= 0) 
                     num_samples += 32; //Wrap condition
                 // Print out number of samples
@@ -139,22 +153,44 @@ int main(void)
                 debug_print(msg);
                 // Read FIFO
                 MAX30101_ReadFIFO(num_samples, active_leds, &data);
-                int samples = data.head - data.tail;
-                sprintf(msg, "%d\r\n", samples);
-                debug_print(msg);
-                for (int i=0;i<samples;i++){
-                    sprintf(msg, "red: %lu\r\n", data.red[data.tail+i]);
+                //int samples = data.head - data.tail;
+                //sprintf(msg, "%d\r\n", samples);
+                //debug_print(msg);
+                for (int i=0;i<num_samples;i++){
+                    sprintf(msg, "red: %lu\r", data.red[data.tail+i]);
                     debug_print(msg);
-                    sprintf(msg, "ir: %lu\r\n", data.IR[data.tail+i]);
+                    sprintf(msg, "ir: %lu\r", data.IR[data.tail+i]);
                     debug_print(msg);
                     //data.tail++;
+                    redBuffer[k] = data.red[data.tail];
+                    irBuffer[k] = data.IR[data.tail];
+                    k++;
+                    if(flag_1s) {
+                        sprintf(msg, "spo2: %ld\r", spo2);
+                        debug_print(msg);
+                        sprintf(msg, "validspo2: %d\r", validSPO2);
+                        debug_print(msg);
+                        sprintf(msg, "HR: %ld\r", heartRate);
+                        debug_print(msg);
+                        sprintf(msg, "validHR: %d\r\n", validHeartRate);
+                        debug_print(msg);   
+                        flag_1s = 0;
+                    }
                 }
-                data.tail=0;
-                data.head=0;
+                //data.tail = 0;
+                //data.head = 0;
             }
-            
             flag_temp = 0;
-        }        
+            if(k==200) {
+                maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+                flag_1s = 1;
+                k=150;
+                for (int i=50; i<200; i++) {
+                    redBuffer[i-50] = redBuffer[i];
+                    irBuffer[i-50] = irBuffer[i];
+                }
+            }
+        }
     }
 }
 
